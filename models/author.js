fd280c2e3cf2551fae7dc016aece17bfec507d39
Alex Wang <jianuo.wang@duke.edu>
2020-06-12 16:38:31 -0500

    author-list

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose")
+var moment = require('moment')
 
 var Schema = mongoose.Schema
 
@@ -43,6 +44,21 @@ AuthorSchema
     .get(function() {
         return "/catalog/author/" + this._id
     })
+
+// Virtual for author's formatted DOB
+AuthorSchema
+    .virtual("date_of_birth_formatted")
+    .get(function() {
+      return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '' 
+    })
+
+// Virtual for author's formatted DOD
+AuthorSchema
+    .virtual("date_of_death_formatted")
+    .get(function() {
+      return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '' 
+    })
+
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema)
