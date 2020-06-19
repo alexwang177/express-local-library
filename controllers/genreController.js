16b274e892087a0c1f9b706ec00c321bfa5d6069
Alex Wang <jianuo.wang@duke.edu>
2020-06-19 13:56:43 -0500

    update genre working

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,6 +3,9 @@ var Book = require('../models/book')
 
 var async = require('async')
 
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
+
 const validator = require('express-validator')
 
 // Display list of all Genre.
@@ -146,11 +149,48 @@ exports.genre_delete_post = function(req, res, next) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function(req, res, next) {
+    Genre.findById(req.params.id, function(err, genre) {
+        if(err) return next(err)
+
+        if(genre == null) {
+            var err = new Error('Genre not found')
+            err.status = 404
+            return next(err)
+        }
+
+        res.render('genre_form', { title: 'Update Genre', genre: genre })
+    })
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+    // Validate fields.
+    body('name', 'Title must not be empty.').trim().isLength({ min: 1 }),
+
+    // Sanitize fields.
+    sanitizeBody('name').escape(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+        
+        const errors = validationResult(req)
+
+        // Create a Genre object with escaped and trimmed data
+        var genre = new Genre({ name: req.body.name, _id:req.params.id })
+
+        if(!errors.isEmpty()) {
+        
+            res.render('genre_form', { title: 'Update Genre', genre: genre, errors: errors.array() })
+            
+        }
+        else {
+            // Data from form is valid. Update the record.
+            Genre.findByIdAndUpdate(req.params.id, genre, {}, function(err, thegenre) {
+                if(err) return next(err)
+
+                res.redirect(thegenre.url)
+            })
+        }
+    }
+]
